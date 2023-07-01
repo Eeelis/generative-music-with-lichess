c6d6d5b02b9d801b05bc33910f9d1ee0337f177d
Eelis Lähteenaro <138326023+Eeelis@users.noreply.github.com>
2023-07-01 23:23:37 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,47 +1,9 @@
 import berserk, requests, json, webbrowser, socket
 
-number_of_games = 0
 token = YOUR-TOKEN-GOES-HERE
+number_of_games = 0
 
-# Send PD a 1 for each occupied square and a 0 for each unoccupied square:
-def Initialize():
-    arr = data["d"]["fen"].split('/')
-    positions = ''
-    for e in arr:
-        for c in e:
-            if (c.isalpha() or c == '-'):
-                positions += '1'
-            else:
-                positions += int(c.strip() or 0) * '0' 
-    print(positions + "\n")
-    for char in positions:
-        s.sendall((char + ";").encode(encoding="utf-8")) # ";" added to signal end of TCP message
-    
-# Setup server for sending data to PD:
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-host = socket.gethostname()
-port = 9001
-s.connect(host, port)
-print("Ready to send data via port " + str(port) + "\nPress enter to continue")
-headers={
-'Authorization': f'Bearer ' + token,
-"Accept": "application/x-ndjson"
-}
-
-# Create session as described at https://berserk.readthedocs.io/en/master/readme.html
-
-
-session = berserk.TokenSession(token)
-client = berserk.Client(session=session)
-
-# Open live game in web browser and receive game info in ndjson:
-
-webbrowser.open("https://lichess.org/tv")
-lines = requests.get('https://lichess.org/api/tv/feed', stream = True, headers=headers).iter_lines()
-
-# Assign each square a value between 2 (a8, top left) and 65 (h1, bottom right). 1 and 0 are reserved for initializing:
-
+# Each square is assigned a value between 2 (a8, top left) and 65 (h1, bottom right), with 1 and 0 are reserved for initializing
 board = {
     "a8": "2;",  "b8": "3;",  "c8": "4;",  "d8": "5;",  "e8": "6;",  "f8": "7;",  "g8": "8;",  "h8": "9;",
     "a7": "10;",  "b7": "11;", "c7": "12;", "d7": "13;", "e7": "14;", "f7": "15;", "g7": "16;", "h7": "17;",
@@ -53,8 +15,40 @@ board = {
     "a1": "58;", "b1": "59;", "c1": "60;", "d1": "61;", "e1": "62;", "f1": "63;", "g1": "64;", "h1": "65;",
     }
 
-# Parse, print and send to PD:
+# Send PD a 1 for each occupied square and a 0 for each unoccupied square
+def Initialize():
+    arr = data["d"]["fen"].split('/')
+    positions = ''
+    for e in arr:
+        for c in e:
+            if (c.isalpha() or c == '-'):
+                positions += '1'
+            else:
+                positions += int(c.strip() or 0) * '0' 
+    print(positions + "\n")
+    for char in positions:
+        s.sendall((char + ";").encode(encoding="utf-8"))
+    
+# Setup server for sending data to PD
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+host = socket.gethostname()
+port = 9001
+s.connect(host, port)
+print("Ready to send data via port " + str(port) + "\nPress enter to continue")
+headers={
+'Authorization': f'Bearer ' + token,
+"Accept": "application/x-ndjson"
+}
 
+# Create session as described at https://berserk.readthedocs.io/en/master/readme.html
+session = berserk.TokenSession(token)
+client = berserk.Client(session=session)
+
+# Open game in web browser and start receiving game info
+webbrowser.open("https://lichess.org/tv")
+lines = requests.get('https://lichess.org/api/tv/feed', stream = True, headers=headers).iter_lines()
+
+# Parse, print and send to PD
 for line in lines:
     data = json.loads(line)
     if (data["t"] == "featured"):
